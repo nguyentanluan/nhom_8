d65322de920275c74e81fb5540c6c4426e295522
nguyentanluan <ntan14435@gmail.com>
2026-06-08 09:59:49 +0700

    Delete yumexpress directory

--- yumexpress/sendmail.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Hàm giả lập gửi mail để không bị lỗi Class PHPMailer
-function sendYumEmail($toEmail, $subject, $body) {
-    // Trả về true để mạch logic ở file forgot_password.php tiếp tục chạy
-    return true; 
-}
-?>
